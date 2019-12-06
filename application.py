fac873cd18099a5182aa0f34f4a6ea9527615f89
Roy <aritro.roy0@gmail.com>
2019-12-05 21:12:47 -0500

    TO Be Uploaded to CS50 IDE

--- application.py
+++ application.py
@@ -41,3 +41,10 @@ def index():
 @app.route("/login")
 def login():
     return render_template("login.html")
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template("register.html")
+    else:
+        return 
